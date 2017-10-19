94fe51546d6070a8243511e72e7030febc1f5a66
Shaun Wahle <shaun.wahle@gmail.com>
2017-10-18 19:39:55 -0700

    Adds working test and I'm pretty close

--- shaun-lab25/lib/read.js
+++ shaun-lab25/lib/read.js
@@ -1,27 +1,35 @@
-'use strict';
+
+'use-strict';
+
 
 const fs = require('fs');
+const readMod = module.exports = {};
 
-const file = module.exports = {};
 
 let nameStack = [];
 let contentStack = [];
-file.read = (cb) => {
-  let fileData;
 
-  fs.readFile(`${__dirname}../../minimal.html`, (err, data) => {
+readMod.read = (callback) => {
+  let parsedData;
+
+  fs.readFile(`${__dirname}/../../assets/minimal.html`, (err, data) => {
     if(err) console.error(err);
-    fileData = data.toString('utf-8');
-    console.log(fileData);
+    parsedData = data.toString('utf-8');
+    console.log(parsedData);
 
-    let dataSplit = fileData.split('/n');
-    console.log(dataSplit, 'Data that is split');
+    let splitData = parsedData.split('/n');
+    console.log(splitData, 'splitData');
 
-    dataSplit.map(ele => {
-      if (ele.includes() === '<head/>/n' || ele.inculdes() === '<header>/n' || ele.includes() === '<body>/n' || 
-        ele.includes() === '<footer>/n' || ele.includes() === '<nav>/n' || ele.includes() === '<main>/n' || 
-        ele.includes() === '<section>/n');
-    })
+    splitData.map(ele => {
+      if (ele.includes() === '<head>/n' || ele.includes() === '<header>/n' || ele.includes() === '<body>/n' || ele.includes() === '<footer>/n' || ele.includes() === '<nav>/n' || ele.includes() === '<main>/n' || ele.includes() === '<section>/n')
+      {
+        nameStack.push(ele);
+      } else if (ele.includes() === '<h2>' || ele.includes() === '<li>' || ele.includes() === '<p>') {
+        contentStack.push(ele);
+      }
+    });
+    console.log(nameStack, contentStack);
   });
-  return cb;
+
+  return callback;
 };
