32f7185738e762d5cb1143940ca3043c1f9cc859
Shaun Wahle <shaun.wahle@gmail.com>
2017-10-18 18:44:04 -0700

    adds partial read file

--- shaun-lab25/lib/read.js
+++ shaun-lab25/lib/read.js
@@ -4,14 +4,24 @@ const fs = require('fs');
 
 const file = module.exports = {};
 
-
+let nameStack = [];
+let contentStack = [];
 file.read = (cb) => {
-  let Filedata;
+  let fileData;
 
-  fs.readFile(`${__dirname}/../minimal.html`, (err, data) => {
+  fs.readFile(`${__dirname}../../minimal.html`, (err, data) => {
     if(err) console.error(err);
-    Filedata = data.toString('utf-8');
-    console.log(Filedata);
+    fileData = data.toString('utf-8');
+    console.log(fileData);
+
+    let dataSplit = fileData.split('/n');
+    console.log(dataSplit, 'Data that is split');
+
+    dataSplit.map(ele => {
+      if (ele.includes() === '<head/>/n' || ele.inculdes() === '<header>/n' || ele.includes() === '<body>/n' || 
+        ele.includes() === '<footer>/n' || ele.includes() === '<nav>/n' || ele.includes() === '<main>/n' || 
+        ele.includes() === '<section>/n');
+    })
   });
   return cb;
 };
