14d5316129fbd9c04044537c92276ddd924f3ba1
Shaun Wahle <shaun.wahle@gmail.com>
2017-10-18 18:04:34 -0700

    adds partial tree test

---
+++ shaun-lab25/__test__/tree.test.js
@@ -0,0 +1,35 @@
+'use strict';
+
+const treeNodeTest = require('../lib/tree');
+
+describe('Tree constructor method', function () {
+
+  describe('#Tree Root constructor', function () {
+    describe('#New Node', function () {
+
+      test('should return a new root tree node', () => {
+        let testTree = new treeNodeTest('head', 'title');
+        expect(testTree.val).toEqual({'eleName': 'head', 'textContent': 'title'});
+
+      });
+    });
+
+    describe('#Insertnewnode', () => {
+      test('should become a child node', () => {
+        let testTree2 = new treeNodeTest('body', 'header, main, footer');
+        let testTreeS = new treeNodeTest('header', 'H2, nav');
+        let testTreeH = new treeNodeTest('main', 'section');
+        let testTreeA = new treeNodeTest('footer', 'p');
+        testTree2.children = [testTreeS, testTreeH, testTreeA];
+        expect(testTree2.children.length).toBe(3);
+      });
+    });
+    describe('#Prune', () => {
+      test('should remove child from given node', () => {
+        
+
+      });
+    })
+
+  });
+});
