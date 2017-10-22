f95b5b44a05d749688a0dee1853c4b5c48739b86
Tim Turner <timmyturner@me.com>
2017-10-21 18:15:31 -0700

    adds files for tree and attempting html parser

--- lab-michelle/lib/htmlBuffer.js
+++
@@ -1,7 +0,0 @@
-'use-strict';
-
-module.exports = function HtmlBuffer (buffer) {
-  this.allData = buffer;
-  this.type = buffer.toString('utf-8', 0, buffer.length);
-  //We should think about converting this to some kind of endian here//
-};
